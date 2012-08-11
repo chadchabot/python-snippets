3b0bdce0ac560a667dae76a68d8bdee979fc0078
Chad Chabot <chadchabot@gmail.com>
2012-08-11 19:11:56 -0400

    Tried to use a Counter instead of a regular dictionary. It didn't work.
    I think this approach of just comparing the # of each letter in each word is better than counting and decrementing, because it doesn't involve nearly as many changes to variables.

--- scrabbleSolver.py
+++ scrabbleSolver.py
@@ -1,25 +1,60 @@
 import sys
 import re
 
+class Counter(dict):
+    def __missing__(self, key):
+        return 0
+
+def buildWord2( dictWord, letterLookupTable ):
+    #    new approach to the buildWord() function
+    letterCount = Counter()
+    for letter in dictWord:
+        letterCount[ letter ] += 1
+    
+    for letter in letterCount:
+        if ( letterLookupTable[ letter ] < letterCount[ letter ] ):
+            return 0
+        else:
+            return 1
+        
+
+
+
 def buildWord( stringToMatch, letterLookupTable ):
 
     for letter in stringToMatch:
         #   new copy of letterLookupTable
-        table = {}
+        table = Counter()
         for key in letterLookupTable:
             table[ key ] = letterLookupTable[ key ]
         #   if the letter is found in table
         if ( letter in table ):
-            #   decrement value of table[letter]
-            table[ letter ] -= 1
-            #   if table[value] == 0
-            if ( table[ letter ] == 0 ):
-                del table[ letter ]
+            if ( table[ letter ] > 0 ):
+                #   decrement value of table[letter]
+                table[ letter ] -= 1
+            else:
+                print str( letter ) + " = " + str( table[ letter ] )
         else:
             return 0
-    
+    print stringToMatch + "\t -- " + str( table )
     return 1
 
+def longestWord( wordList ):
+    length = 0
+    longestWord = []
+    
+    for word in wordList:
+        #   here is where I would add in a calculator for word value
+        wordLength = len( word )
+        if ( wordLength == length ):
+            #   add to list
+            longestWord.append( word )
+        elif ( wordLength > length ):
+            del longestWord[:]
+            length = len( word )
+            longestWord.append( word )
+
+    return longestWord
 
 def main():
     if ( len( sys.argv ) < 2 ):
@@ -36,17 +71,14 @@ def main():
         sys.exit( 1 )
 
     #   build player hand
-    playerHand = {}
+    playerHand = Counter()
     playerHandLength = len( string )
     for letter in string:
         #   add to lookup table
-        if ( letter in playerHand ):
-            playerHand[ letter ] += 1
-        else:
-            playerHand[ letter ] = 1
+        playerHand[ letter ] += 1
     
-    for key in playerHand:
-        print key + " = " + str( playerHand[ key ] )
+#    for key in playerHand:
+#        print key + " = " + str( playerHand[ key ] )
 
     #   load dictionary file
     with open( "/usr/share/dict/words" ) as fp:
@@ -54,7 +86,7 @@ def main():
     
     #   need a place to store words that can be made from input string
     wordList = []
-
+    print "\t -- " + str( playerHand )
     #   go through each word in the dict file
     for word in dict_file:
         #   trim the newline character
@@ -64,11 +96,17 @@ def main():
         #   use a system call (grep?) and pipe results to dict_file?
         if ( len( word ) <= playerHandLength ):
             #   check if 'word' can be built using 'playerHand'
-            if ( 1 == buildWord( word, playerHand ) ):
+            if ( 1 == buildWord2( word, playerHand ) ):
                 #   add to wordList
                 wordList.append( word )
     
-    print wordList
+#    print wordList
+    #   check for longest word
+    longest = longestWord( wordList )
+    
+    print "The longest word(s) to be made given [" + string + "]:"
+    for word in longest:
+        print word
         
 
 if __name__ == '__main__':
