1e244c8f3f5ddc692572480756c6b9e01cf28531
Chad Chabot <chadchabot@gmail.com>
2012-08-11 18:44:15 -0400

    Added buildWord() to test if a word from the dictionary can be built using the player's hand.
    Testing for errors in buildWord().

--- scrabbleSolver.py
+++ scrabbleSolver.py
@@ -1,7 +1,23 @@
 import sys
 import re
 
-#   for letter in string
+def buildWord( stringToMatch, letterLookupTable ):
+
+    for letter in stringToMatch:
+        #   new copy of letterLookupTable
+        table = letterLookupTable
+        #   if the letter is found in table
+        if ( letter in table ):
+            #   decrement value of table[letter]
+            table[ letter ] -= 1
+            #   if table[value] == 0
+            if ( table[ letter ] == 0 ):
+                del table[ letter ]
+        else:
+            return 0
+    
+    return 1
+
 
 def main():
     if ( len( sys.argv ) < 2 ):
@@ -19,6 +35,7 @@ def main():
 
     #   build player hand
     playerHand = {}
+    playerHandLength = len( string )
     for letter in string:
         #   add to lookup table
         if ( letter in playerHand ):
@@ -30,7 +47,27 @@ def main():
         print key + " = " + str( playerHand[ key ] )
 
     #   load dictionary file
+    with open( "/usr/share/dict/words" ) as fp:
+        dict_file = fp.readlines()
     
+    #   need a place to store words that can be made from input string
+    wordList = []
+
+    #   go through each word in the dict file
+    for word in dict_file:
+        #   trim the newline character
+        word = word[:-1]
+        #   is 'word' longer than the length of input string?
+        #   can this check be combined with the readlines() or dict_file building step?
+        #   use a system call (grep?) and pipe results to dict_file?
+        if ( len( word ) <= playerHandLength ):
+            #   check if 'word' can be built using 'playerHand'
+            if ( 1 == buildWord( word, playerHand ) ):
+                #   add to wordList
+                wordList.append( word )
+    
+    print wordList
+        
 
 if __name__ == '__main__':
     main()
